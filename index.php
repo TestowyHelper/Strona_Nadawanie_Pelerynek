ce818a487effd300560bcb280ad10358c688a94a
TestowyHelper <126357499+TestowyHelper@users.noreply.github.com>
2023-02-26 15:38:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Strona_Nadawanie_Pelerynek</title>
+        <link rel="stylesheet" href="style.css">
+</head>
+<body>
+        <form method="post">
+                <input type="text" name="nick" placeholder="Wprowadź Nick">
+                <input type="text" name="id" placeholder="Wprowadź ID">
+                <button type="submit" name="nadaj">Nadaj</button>
+        </form>
+</body>
+</html>
+
+
+<?php
+if (isset($_POST['nadaj'])) {
+$n = $_POST['nick'];
+$i = $_POST['id'];
+
+
+        $capes = 'C:/xampp/htdocs/cape/'.$n.'.png';
+        $list = 'C:/xampp/htdocs/getcape/'.$i.'.png';
+                if (!copy($list, $capes)) {
+                        echo "blad";
+                }
+
+}
+?>
